4aa74f0a5866851488202ee58e269dd3cd5c7698
djmsoftware53 <djmsoftware@bluewin.ch>
2020-05-04 17:06:24 +0200

    Version initiale

---
+++ Unit10.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit10H
+#define Unit10H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm10 : public TForm
+{
+__published:	// Composants gérés par l'EDI
+private:	// Déclarations utilisateur
+public:		// Déclarations utilisateur
+	__fastcall TForm10(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm10 *Form10;
+//---------------------------------------------------------------------------
+#endif
